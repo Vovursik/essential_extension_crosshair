247d0443f6d697d6a49ab60e1823b03149e3bfbe
Vladimir <vovavaisik.lyz@gmail.com>
2026-08-15 21:19:03 +0300

    fix

--- src-tauri/src/tray.rs
+++ src-tauri/src/tray.rs
@@ -1,7 +1,7 @@
 use tauri::{
     Manager,
     tray::TrayIconBuilder,
-    menu::{Menu, IconMenuItem},
+    menu::{Menu, IconMenuItem, MenuItem},
     image::Image,
 };
 use tauri_plugin_dialog::DialogExt;
@@ -9,24 +9,16 @@ use tauri_plugin_dialog::DialogExt;
 use crate::crosshair;
 
 pub fn create_tray(app: &tauri::AppHandle) {
-    let icon_path = app.path()
-        .resolve("icons/32x32.png", tauri::path::BaseDirectory::Resource)
-        .unwrap();
-    let load_icon_path = app.path()
-        .resolve("icons/tray/load.png", tauri::path::BaseDirectory::Resource)
-        .unwrap();
-    let quit_icon_path = app.path()
-        .resolve("icons/tray/quit.png", tauri::path::BaseDirectory::Resource)
-        .unwrap();
+    let load_item = MenuItem::with_id(app, "load", "Load Crosshair", true, None::<&str>).unwrap();
+    let quit_item = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>).unwrap();
 
-    let icon = Image::from_path(icon_path).unwrap();
-    let load_icon = Image::from_path(load_icon_path).unwrap();
-    let quit_icon = Image::from_path(quit_icon_path).unwrap();
+    let icon = Image::from_bytes(include_bytes!("../icons/32x32.png")).unwrap();
+    let load_icon = Image::from_bytes(include_bytes!("../icons/tray/load.png")).unwrap();
+    let quit_icon = Image::from_bytes(include_bytes!("../icons/tray/quit.png")).unwrap();
 
     let load_item = IconMenuItem::with_id(
-        app, "load", "Load", true, Some(load_icon), None::<&str>,
+        app, "load", "Load Crosshair", true, Some(load_icon), None::<&str>,
     ).unwrap();
-
     let quit_item = IconMenuItem::with_id(
         app, "quit", "Quit", true, Some(quit_icon), None::<&str>,
     ).unwrap();
@@ -39,7 +31,6 @@ pub fn create_tray(app: &tauri::AppHandle) {
         .on_menu_event(move |app, event| match event.id.as_ref() {
             "load" => {
                 let app_handle = app.clone();
-
                 app.dialog()
                     .file()
                     .add_filter("", &["zip"])
